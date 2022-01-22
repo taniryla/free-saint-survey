ca02922d0580a95bce27fd52d7bb9e1def5e8037
taniryla <ryland0000@gmail.com>
2022-01-21 17:35:54 -0700

    initial express mongoose set-up

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var passport = require('passport');
+
+require('dotenv').config();
+// Connect to the MongoDB database
+require('./config/database');
+// Configure Passport
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var surveysRouter = require('./routes/surveys');
 
 var app = express();
 
@@ -18,9 +27,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
+const isLoggedIn = require('./config/auth');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/surveys', surveysRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
