8bc415b51201bb3d2e729c6d58d10fe932747f2f
taniryla <ryland0000@gmail.com>
2022-01-24 19:43:26 -0700

    completed survey questions 1 through 4

--- controllers/surveys.js
+++ controllers/surveys.js
@@ -2,6 +2,12 @@ const Survey = require('../models/survey');
 
 module.exports = {
   index,
+  view2,
+  view2create,
+  view3,
+  view3create,
+  view4,
+  view4create,
   create
 };
 
@@ -13,6 +19,48 @@ function index(req, res) {
   .catch(function(err) {
     res.redirect('/surveys');
   });
+}
+
+function view2(req, res) {
+  res.render('surveys/view2', { surveys });
+}
+
+function view2create(req, res) {
+  const survey = new Survey(req.body);
+  survey.save(function (err) {
+    // one way to handle errors
+    if (err) return res.render('surveys/view2');
+    console.log(survey);
+  res.render(`surveys/view2`, { survey });
+});
+}
+
+function view3(req, res) {
+  res.render('surveys/view3', { surveys });
+}
+
+function view3create(req, res) {
+  const survey = new Survey(req.body);
+  survey.save(function (err) {
+    // one way to handle errors
+    if (err) return res.render('surveys/view3');
+    console.log(survey);
+  res.render(`surveys/view3`, { survey });
+});
+}
+
+function view4(req, res) {
+  res.render('surveys/view4', { surveys });
+}
+
+function view4create(req, res) {
+  const survey = new Survey(req.body);
+  survey.save(function (err) {
+    // one way to handle errors
+    if (err) return res.render('surveys/view4');
+    console.log(survey);
+  res.render(`surveys/view4`, { survey });
+});
 }
 
 function create(req, res) {
--- routes/surveys.js
+++ routes/surveys.js
@@ -9,6 +9,24 @@ var isLoggedIn = require('../config/auth');
 // GET /surveys (start of the survey funnel and display a form for entering a new survey info into database)
 router.get('/', surveysCtrl.index);
 
+// GET /surveys/view2 
+router.get('/view2', surveysCtrl.view2)
+
+// POST /surveys/view2 
+router.post('/view2', surveysCtrl.view2create)
+
+// GET /surveys/view3 
+router.get('/view3', surveysCtrl.view3)
+
+// POST /surveys/view3 
+router.post('/view3', surveysCtrl.view3create)
+
+// GET /surveys/view4 
+router.get('/view4', surveysCtrl.view4)
+
+// POST /surveys/view3 
+router.post('/view4', surveysCtrl.view4create)
+
 // POST /surveys (handles the new form being submitted)
 router.post('/', surveysCtrl.create);
 
